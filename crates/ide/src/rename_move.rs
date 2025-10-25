5d1b8495feb3532bb14681d0692966f2e56b866a
Michael Adams <madams2419@gmail.com>
2025-10-24 23:07:05 -0400

    wip

--- crates/ide/src/rename_move.rs
+++ crates/ide/src/rename_move.rs
@@ -7,5 +7,40 @@ pub(crate) fn rename_move(
     position: FilePosition,
     new_name: &str,
 ) -> RenameResult<SourceChange> {
+    // TODO:
+    //
+    // Compilation:
+    // - Parse the mod path
+    // - Find the definition, ensure it can be renamed
+    // - Collect inbound references (in_refs)
+    // - Collect outbound references (out_refs)
+    //
+    // Validation:
+    // - Determine minimum visibility required of the struct after move based on inbound references
+    // - Ensure all internal refs are still accessible
+    // - If destination module already exists, ensure new name won't clash with an existing name
+    //
+    // New module changes:
+    // - Create module if it doesn't exist
+    // - Copy over text range of move item
+    // - Satisfy out_refs (resolve imports)
+    //   - Insert use items for all out_refs
+    //   - De-dup against existing use statements
+    // - Satisfy in_refs
+    //   - Change visibility of definition + decords
+    //   - Update external use statements (simple rename)
+    //
+    // Old module changes:
+    // - Remove any use statements that only satisfied imports within moved selection
+    //
+    // Other module changes:
+    // - Update external use statements (simple rename)
+    //
+    // struct OutRef {
+    //    used_in_original_mod: bool,
+    //    used_in_new_mod: bool,
+    //
+    // }
+    //
     todo!()
 }
